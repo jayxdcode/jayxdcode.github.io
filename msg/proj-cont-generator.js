887e85955ca457af09b57d9046f22c14f4790ff0
jayxdcode <alt444.001@gmail.com>
2025-04-20 20:15:40 +0800

    update

--- msg/proj-cont-generator.js
+++ msg/proj-cont-generator.js
@@ -1,29 +1,112 @@
 const username = "jayxdcode";
-const BASE_URL = "https://api.github.com/"
+const DATA_URL = `${username}_data.json`;
 
-const ENDPOINT = `${BASE_URL}users/${username}/repos`;
-
-let CACHED_GITHUB_DATA = localStorage.getItem(`githubRepoCache_${username}`);
-
-if (CACHED_GITHUB_DATA) {
-  alert(CACHED_GITHUB_DATA)
+function formatUTCDateString(utcDateString) {
+  const date = new Date(utcDateString);
+  const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: '2-digit', hour12: true };
+  return date.toLocaleDateString('en-US', options).replace(',', '');
 }
 
-fetch(ENDPOINT)
+fetch(DATA_URL)
   .then(response => {
-    // Handle the response here
-    console.log('Response received:', response);
-    // You'll typically need to process the response body (e.g., as JSON)
-    return response.json(); // Returns another Promise
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
   })
   .then(data => {
-    // Use the data from the response body
-    GITHUB_DATA = data;
-    console.log('Data:', data);
+    console.log("Data fetched successfully:", data);
     
+    const pinnedData = data.pinnedRepos;
+    const pinned = document.querySelector("div#pinnedRepos");
+    pinned.querySelectorAll(".sectionItem").forEach(item => item.remove());
     
+    for (let i = 0; i < pinnedData.length; i++) {
+      const repo = pinnedData[i];
+      
+      const name = repo.name || "No name";
+      const url = repo.url || "#";
+      const desc = repo.description ?? "No description provided.";
+      const stars = repo.stars ?? 0;
+      const forks = repo.forks ?? 0;
+      const updated = formatUTCDateString(repo.updatedAt) ?? "Date Unknown";
+      
+      const lineBreak = document.createElement("div");
+      lineBreak.className = "lineBreak";
+      
+      lineBreak.style.width = "calc(100% - 4em)";
+      lineBreak.style.justifySelf = "center";
+      
+      pinned.appendChild(lineBreak);
+      
+      const repoDiv = document.createElement("div");
+      repoDiv.className = "sectionItem";
+      repoDiv.id = `a${i}`;
+      
+      repoDiv.innerHTML = `
+			<a class="repoItem" href="${url}" target="_blank">
+				<div class="repoTitle">${name}</div>
+				<div class="repoDesc">${desc}</div>
+				<div class="repoStats">
+					<span class="stars">★ ${stars}</span>
+					<span class="forks">⑂ ${forks}</span>
+				</div>
+				<div class="upd">
+          <span class="updated">Updated: ${updated}</span>
+				</div>
+			</a>
+		`;
+      
+      pinned.appendChild(repoDiv);
+    }
+    
+    const latestMods = data.latestModifiedRepos;
+    const latest = document.querySelector("div#latestMods");
+    latest.querySelectorAll(".sectionItem").forEach(item => item.remove());
+    
+    for (let i = 0; i < latestMods.length; i++) {
+      const repo = latestMods[i];
+      
+      const name = repo.name || "No name";
+      const desc = repo.description ?? "No description provided.";
+      const url = repo.url || "#";
+      const updated = formatUTCDateString(repo.updatedAt) ?? "Date Unknown";
+      
+      const lineBreak = document.createElement("div");
+      lineBreak.className = "lineBreak";
+      
+      lineBreak.style.width = "calc(100% - 4em)";
+      lineBreak.style.justifySelf = "center";
+      
+      latest.appendChild(lineBreak);
+      
+      const repoDiv = document.createElement("div");
+      repoDiv.className = "sectionItem";
+      repoDiv.id = `a${i}`;
+      
+      repoDiv.innerHTML = `
+			<a class="repoItem" href="${url}" target="_blank">
+				<div class="repoTitle">${name}</div>
+				<div class="repoDesc">${desc}</div>
+				<div class="upd">
+          <span class="updated">Updated: ${updated}</span>
+				</div>
+			</a>
+		`;
+      
+      latest.appendChild(repoDiv);
+    }
   })
   .catch(error => {
-    // Handle any errors that occurred during the fetch operation
-    console.error('Fetch error:', error);
+    console.error("An error has occurred while fetching data: ", error);
   });
+
+document.querySelectorAll(".feature-down").forEach(item => {
+  const downOverlay = document.createElement("div");
+  downOverlay.className = "down-overlay";
+  
+  downOverlay.innerHTML = `
+		<div class="down-overlay-text">FEATURE UNAVAILABLE AT THE MEANTIME.</div>`;
+  
+  item.appendChild(downOverlay);
+});
